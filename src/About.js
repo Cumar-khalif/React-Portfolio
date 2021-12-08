8ea0179ea8c5f09c88e299dc2c39ef1e04613e65
omar khalif <73026620+Cumar-khalif@users.noreply.github.com>
2021-12-07 18:50:18 -0600

    uploaded images(my project images), added about me component that containts things that i would like to share

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import  './About.css';
-import aboutImg from "./img/about.jpg"
+import aboutImg from './imgs/about.png';
 
 function About() {
     return (
@@ -8,8 +8,33 @@ function About() {
           <div className="container">
             <div className="row">
               <div className="col__2">
-                <img src={about} alt="" className="about__img" />
+                <img src={aboutImg} alt="" className="about__img" />
+              </div>
+              <div className="col__2">
+                <h1 className="about__heading">
+                  About Me
+                </h1>
+                  <div className="about__meta">
+                    <p className="about__text p__color">
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                    </p>
+                    <p className="about__text p__color">
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                    </p>
+                    <p className="about__text p__color">
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                        lorem ipsum dolor sit amet, lorem ipsum dolor.lorem ipsum dolor sit amet, lorem ipsum dolor
+                    </p>
+                    <div className="about__button d__flex align__items__center">
+                      <a href="#"><button className="about btn pointer">Resume</button></a>
 
+                    </div>
+                  </div>
               </div>
 
             </div>
